0cd949113f41c91cc17fe7fab226344c2260025e
kanekodaichi <knkdic.bfc@gmail.com>
2021-01-27 23:03:53 +0900

    UserモデルとReportモデルのモデルテスト&日報のシステムテストを作成

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,13 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#created_on" do
+    report = Report.create!(user: users(:alice), title: 'sample', content: 'sample')
+    assert_equal report.created_at.to_date, report.created_on
+  end
+
+  test "#editable?" do
+    report = Report.create!(user: users(:alice), title: 'sample', content: 'sample')
+    assert report.editable?(users(:alice))
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,58 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @me = User.create!(email: 'me@example.com', password: 'password')
+    @she = User.create!(email: 'she@example.com', password: 'password')
+    @he = User.create!(email: 'he@example.com', password: 'password')
+  end
+  
+  test "#name_or_email" do
+    user = User.new(email: 'foo@example.com', name: '')
+    #メールアドレスが返ってくれば成功
+    assert_equal 'foo@example.com', user.name_or_email
+
+    user.name = 'Foo Bar'
+    #名前が返ってくれば成功
+    assert_equal 'Foo Bar', user.name_or_email
+  end
+
+  test "#follow" do
+    #フォローしていなければ成功
+    assert_not @me.following?(@she)
+
+    @me.follow(@she)
+    #フォローしていれば成功
+    assert @me.following?(@she)
+  end
+
+  test "#unfollow" do
+    @me.follow(@she)
+    #フォローしていれば成功
+    assert @me.following?(@she)
+    
+    @me.unfollow(@she)
+    #フォローしていなければ成功
+    assert_not @me.following?(@she)
+  end
+
+  test "#following?" do
+    assert_not @me.following?(@she)
+    assert_not @me.following?(@he)
+
+    @me.follow(@she)
+
+    assert @me.following?(@she)
+    assert_not @me.following?(@he)
+  end
+
+  test "#followed_by?" do
+    assert_not @me.followed_by?(@she)
+    assert_not @me.followed_by?(@he)
+
+    @she.follow(@me)
+
+    assert @me.followed_by?(@she)
+    assert_not @me.followed_by?(@he)
+  end
 end
